f4ef9af990b0706670ff4224058cd2e52798da17
Armaan Gupta <ohayouarmaan@gmail.com>
2023-07-05 21:59:33 +0530

    Added the home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
+import { AnimatePresence, motion } from 'framer-motion'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return <AnimatePresence mode='wait'>
+    <motion.div
+      initial={{ opacity: 0, clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%' }}
+      animate={{ opacity: 1, clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%'}}
+      exit={{ opacity: 0, clipPath: 'polygon(50% 0, 50% 0, 50%% 100%, 50%% 100%' }}
+      transition={{ duration: 1.25 }}
+      key={router.route}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  </AnimatePresence>
 }
---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <main className="bg-[#001C30] h-[100vh] w-[100vw]">
+
+        </main>
+    )
+}
